6829ae54ed0c67d3b5bc3a6a54dc9e830047330c
EARNascimento <eduardo90909@outlook.com>
2025-06-02 19:56:34 -0300

    Criando o construtor e sobrescrevendo as propriedades da classe BaseController

--- Parte1/Parte1/Base/BaseController.cs
+++ Parte1/Parte1/Base/BaseController.cs
@@ -1,1 +1,15 @@
-﻿namespace Parte1.Base;
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parte1.Base;
+
+public abstract class BaseController
+{
+    public virtual List<string> AcoesProduto { get; } = new List<string> { "Pesquisar", "Salvar" };
+    public virtual (string NomeTela, string CodigoModulo) InformacoesProduto { get; } = ("CADASTRO_PRODUTO", "PRD001");
+
+    protected BaseController()
+    {
+        Console.WriteLine("BaseController instanciado");
+    }
+}
