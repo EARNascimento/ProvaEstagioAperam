ae6b8626a447846a3b1dfb1c2a973ec7b09d1c10
EARNascimento <eduardo90909@outlook.com>
2025-06-02 20:17:08 -0300

    Corrigindo as strings das propriedades

--- Parte1/Parte1/Base/BaseController.cs
+++ Parte1/Parte1/Base/BaseController.cs
@@ -5,8 +5,8 @@ namespace Parte1.Base;
 
 public abstract class BaseController
 {
-    public virtual List<string> AcoesProduto { get; } = new List<string> { "Pesquisar", "Salvar" };
-    public virtual (string NomeTela, string CodigoModulo) InformacoesProduto { get; } = ("CADASTRO_PRODUTO", "PRD001");
+    public virtual List<string> AcoesProduto { get; } = new List<string> { "Visualizar", "Editar" };
+    public virtual (string NomeTela, string CodigoModulo) InformacoesProduto { get; } = ("TELA_PADRAO", "MOD000");
 
     protected BaseController()
     {
