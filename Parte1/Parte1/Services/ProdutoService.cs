41db6d99ffe0b9235dd9f3e407ee55392416d80a
EARNascimento <eduardo90909@outlook.com>
2025-06-02 19:52:14 -0300

    Criando o construtor da classe ProdutoService e adicionando um método de teste

--- Parte1/Parte1/Services/ProdutoService.cs
+++ Parte1/Parte1/Services/ProdutoService.cs
@@ -1,1 +1,15 @@
 ﻿namespace Parte1.Services;
+
+public class ProdutoService
+{
+    public ProdutoService()
+    {
+        Console.WriteLine("ProdutoService instanciado");
+    }
+
+    //Teste de Método
+    public string ObterDadosDoProduto(int id)
+    {
+        return $"Dados do Produto com ID: {id}";
+    }
+}
